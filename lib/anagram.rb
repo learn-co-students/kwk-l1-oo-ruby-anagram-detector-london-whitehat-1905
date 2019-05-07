a0f3e6649e1e5aba44788492f8f2fdb0ae27c2c6
NJ-JayJay <metalmedley1@gmail.com>
2019-05-07 21:52:16 +0100

    done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+class Anagram
+
+    def initialize(string)
+        @string = string
+        @result = []
+    end
+    
+    def match(t_arr)
+        t_arr.each do |x|
+            #if x.reverse == @string
+            if x.chars.sort == @string.chars.sort
+                #split("") also works instead of #chars
+                @result << x
+            end
+        end
+        @result
+    end
+end
